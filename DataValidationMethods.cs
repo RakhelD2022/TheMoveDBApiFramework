0cf902f91655ef64ddaa529741bba8d6200adbb7
RakhelD2022 <rakh.dash1001@gmail.com>
2022-11-20 10:47:11 +0530

    Add project files.

---
+++ DataValidationMethods.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDBAPITesting
+{
+    public class DataValidationMethods
+    {
+        public class GuestStars
+        {
+            public string character { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Episodes
+        {
+            public GuestStars GuestStars { get; set; }
+            public string SeasonNumber { get; set; }
+        }
+
+        var data = JsonConvert.DeserializeObject<RootObject>(myJSON);
+        nameArticles=data.articles.FirstOrDefault().description;
+        MessageBox.Show(nameArticles);
+    }
+}
---
+++ MovieDBAPI/MovieDBAPI_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.MovieDB;
+
+namespace MovieDBAPITesting.MovieDBAPI
+{
+    public class MovieDBAPI_Tests
+    {
+        [Test]
+        [Category("APITests")]
+        public async Task API_Test()
+        {
+            Services.MovieDB.MovieDBAPI movieDBAPI = new Services.MovieDB.MovieDBAPI();
+            var reasonPhrase = await movieDBAPI.Get_ReasonPhrase();
+
+            Assert.AreEqual("OK", reasonPhrase);
+        }
+
+        [Test]
+        [Category("APITests")]
+        public async Task ValidateGOTActor()
+        {
+            Services.MovieDB.MovieDBAPI movieDBAPI = new Services.MovieDB.MovieDBAPI();
+            var httpResponse = movieDBAPI.Get_Body();
+            
+            Assert.IsNotNull(httpResponse);
+        }
+    }
+}
